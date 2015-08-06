b81dfb49a0b9d550cd4d7db8005c654451f93a0b
juno <juno@krasava.fi>
2015-08-06 11:25:31 +0300

    prod

--- usefull.bash
+++ usefull.bash
@@ -118,15 +118,19 @@ bin/contents_feeder_redis --site=kotirouvat.com --deltahours=50 && sleep 90 && b
 bin/contents_feeder_redis --site=alastonkuvia.info --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=www.alastonkuvia.info --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=chat.alastonkuvia.info --deltahours=50
 bin/contents_feeder_redis --site=sinkkutreffit.org --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=www.sinkkutreffit.org --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=chat.sinkkutreffit.org --deltahours=50 
 bin/contents_feeder_redis --site=alaston.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=www.alaston.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=chat.alaston.fi--deltahours=50
-
 bin/contents_feeder_redis --site=pimppi.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=www.pimppi.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=chat.pimppi.fi --deltahours=50
-
 bin/contents_feeder_redis --site=alastomat.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=www.alastomat.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=chat.alastomat.fi --deltahours=50
-
 bin/contents_feeder_redis --site=seksivideo.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=www.seksivideo.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=chat.seksivideo.fi --deltahours=50
 
 
 
+bin/contents_feeder_redis --site=runkkaus.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=www.runkkaus.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=chat.runkkaus.fi --deltahours=50
+
+bin/contents_feeder_redis --site=karvainen.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=www.karvainen.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=chat.karvainen.fi --deltahours=50
+
+
+
+bin/contents_feeder_redis --site=sub.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=www.sub.fi --deltahours=50 && sleep 90 && bin/contents_feeder_redis --site=chat.sub.fi --deltahours=50
 
 
 
