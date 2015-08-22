b4389038c477c3395bbc42b41379144beb0d90b3
juno <juno@krasava.fi>
2015-08-22 07:43:06 +0300

    dev

---
+++ src/contents_feeder_redis/find_all_keywords/find_all_keywords_test.go
@@ -0,0 +1,41 @@
+package find_all_keywords
+
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"log"
+	"log/syslog"
+	"testing"
+)
+
+func TestFindAllKeywords(t *testing.T) {
+
+	golog, err := syslog.New(syslog.LOG_ERR, "golog")
+
+	defer golog.Close()
+	if err != nil {
+		log.Fatal("error writing syslog!!")
+	}
+
+	redisprotocol := "tcp"
+	redishost := ":6379"
+
+	c, err := redis.Dial(redisprotocol, redishost)
+	if err != nil {
+
+		golog.Crit(err.Error())
+
+	}
+	defer c.Close()
+
+	keywords := FindAllKeywords(*golog, c, "fi_FI", "porno")
+
+	fmt.Println("len", len(keywords))
+
+	for i, keyword := range keywords {
+
+		fmt.Println(i, keyword)
+
+	}
+
+}
