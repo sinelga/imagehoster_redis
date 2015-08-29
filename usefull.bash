b29415a3077498aafe73ea9971eb7c74ea46a9bc
juno <juno@krasava.fi>
2015-08-29 07:09:16 +0300

    prod

--- usefull.bash
+++ usefull.bash
@@ -163,17 +163,17 @@ bin/contents_feeder_redis --site=sperma.fi && sleep 90 && bin/contents_feeder_re
 bin/contents_feeder_redis --site=pimpin.fi && sleep 90 && bin/contents_feeder_redis --site=www.pimpin.fi && sleep 90 && bin/contents_feeder_redis --site=chat.pimpin.fi
 bin/contents_feeder_redis --site=haluaa.fi && sleep 90 && bin/contents_feeder_redis --site=www.haluaa.fi && sleep 90 && bin/contents_feeder_redis --site=chat.haluaa.fi
 bin/contents_feeder_redis --site=kauniita.fi && sleep 90 && bin/contents_feeder_redis --site=www.kauniita.fi && sleep 90 && bin/contents_feeder_redis --site=chat.kauniita.fi
-
-
-
-
 bin/contents_feeder_redis --site=kullin.fi && sleep 90 && bin/contents_feeder_redis --site=www.kullin.fi && sleep 90 && bin/contents_feeder_redis --site=chat.kullin.fi
-
 bin/contents_feeder_redis --site=pilluvideot.fi && sleep 90 && bin/contents_feeder_redis --site=www.pilluvideot.fi && sleep 90 && bin/contents_feeder_redis --site=chat.pilluvideot.fi
-
 bin/contents_feeder_redis --site=tissikuvat.fi && sleep 90 && bin/contents_feeder_redis --site=www.tissikuvat.fi && sleep 90 && bin/contents_feeder_redis --site=chat.tissikuvat.fi
-
 bin/contents_feeder_redis --site=erotiikkaa.fi && sleep 90 && bin/contents_feeder_redis --site=www.erotiikkaa.fi && sleep 90 && bin/contents_feeder_redis --site=chat.erotiikkaa.fi
+
+
+bin/contents_feeder_redis --site=kertomukset.fi && sleep 90 && bin/contents_feeder_redis --site=www.kertomukset.fi && sleep 90 && bin/contents_feeder_redis --site=chat.kertomukset.fi
+
+bin/contents_feeder_redis --site=ilmaisporno.fi && sleep 90 && bin/contents_feeder_redis --site=www.ilmaisporno.fi && sleep 90 && bin/contents_feeder_redis --site=chat.ilmaisporno.fi
+
+bin/contents_feeder_redis --site=pornotuubi.fi && sleep 90 && bin/contents_feeder_redis --site=www.pornotuubi.fi && sleep 90 && bin/contents_feeder_redis --site=chat.pornotuubi.fi
 
 
 
