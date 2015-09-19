2253ed9048bffb4022883834e53da4ccd8015405
juno <juno@krasava.fi>
2015-09-19 07:49:28 +0300

    prod

--- usefull.bash
+++ usefull.bash
@@ -169,11 +169,13 @@ bin/contents_feeder_redis --site=pikkupillu.fi && sleep 90 && bin/contents_feede
 bin/contents_feeder_redis --site=transu.fi && sleep 90 && bin/contents_feeder_redis --site=www.transu.fi && sleep 90 && bin/contents_feeder_redis --site=chat.transu.fi
 bin/contents_feeder_redis --site=allfreexxx.com && sleep 90 && bin/contents_feeder_redis --site=www.allfreexxx.com && sleep 90 && bin/contents_feeder_redis --site=chat.allfreexxx.com
 bin/contents_feeder_redis --site=klickhere.mobi && sleep 90 && bin/contents_feeder_redis --site=www.klickhere.mobi && sleep 90 && bin/contents_feeder_redis --site=chat.klickhere.mobi
-
-
 bin/contents_feeder_redis --site=bestsex.cc && sleep 90 && bin/contents_feeder_redis --site=www.bestsex.cc && sleep 90 && bin/contents_feeder_redis --site=chat.bestsex.cc
-
 bin/contents_feeder_redis --site=mostpopularsites.org && sleep 90 && bin/contents_feeder_redis --site=www.mostpopularsites.org && sleep 90 && bin/contents_feeder_redis --site=chat.mostpopularsites.org
+
+#common44ang accept other bots 
+
+
+bin/contents_feeder_redis --site=karvaisia.fi && sleep 90 && bin/contents_feeder_redis --site=www.karvaisia.fi && sleep 90 && bin/contents_feeder_redis --site=chat.karvaisia.fi
 
 
 
