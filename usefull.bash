007050abeceab161a0c5b20b06f54f5ce2b2bd4b
alex <support@mazurov.eu>
2015-10-17 08:20:37 +0300

    prod

--- usefull.bash
+++ usefull.bash
@@ -186,5 +186,6 @@ bin/contents_feeder_redis --site=sexs.fi && sleep 90 && bin/contents_feeder_redi
 #common45ang accept other bots 
 bin/contents_feeder_redis --site=paljaat.fi && sleep 90 && bin/contents_feeder_redis --site=www.paljaat.fi && sleep 90 && bin/contents_feeder_redis --site=chat.paljaat.fi
 bin/contents_feeder_redis --site=naiminen.fi && sleep 90 && bin/contents_feeder_redis --site=www.naiminen.fi && sleep 90 && bin/contents_feeder_redis --site=chat.naiminen.fi
-
 bin/contents_feeder_redis --site=teinipillu.fi && sleep 90 && bin/contents_feeder_redis --site=www.teinipillu.fi && sleep 90 && bin/contents_feeder_redis --site=chat.teinipillu.fi
+
+bin/contents_feeder_redis --site=lespo.fi && sleep 90 && bin/contents_feeder_redis --site=www.lespo.fi && sleep 90 && bin/contents_feeder_redis --site=chat.lespo.fi
