5d3bc729c9c7603eedea80ff52fbf5a3a4e4f53c
alex <support@mazurov.eu>
2015-10-27 06:30:18 +0200

    prod

---
+++ changenambers.bash
@@ -0,0 +1,7 @@
+bin/changeallphnumbers -from=0600411464 -to=0600414113
+bin/changeallphnumbers -from=0600411465 -to=0600414114
+bin/changeallphnumbers -from=0600411466 -to=0600414115
+bin/changeallphnumbers -from=0600411467 -to=0600414116
+bin/changeallphnumbers -from=0600411468 -to=0600414117
+bin/changeallphnumbers -from=0600411469 -to=0600414118
+bin/changeallphnumbers -from=0700411308 -to=0600414119
