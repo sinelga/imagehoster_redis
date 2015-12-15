4f7c6827ef4302e78274a5085607c09b8e09b9ec
juno <juno@krasava.fi>
2015-12-15 08:22:11 +0200

    prod

--- usefull.bash
+++ usefull.bash
@@ -227,5 +227,6 @@ bin/contents_feeder_redis --site=pimpsa.fi && sleep 90 && bin/contents_feeder_re
 bin/contents_feeder_redis --site=eroottista.fi && sleep 90 && bin/contents_feeder_redis --site=seksi.eroottista.fi && sleep 90 && bin/contents_feeder_redis --site=chatti.eroottista.fi
 bin/contents_feeder_redis --site=sexivideot.fi && sleep 90 && bin/contents_feeder_redis --site=seksi.sexivideot.fi && sleep 90 && bin/contents_feeder_redis --site=video.sexivideot.fi
 bin/contents_feeder_redis --site=teinien.fi && sleep 90 && bin/contents_feeder_redis --site=seksi.teinien.fi && sleep 90 && bin/contents_feeder_redis --site=sexi.teinien.fi
-
 bin/contents_feeder_redis --site=pornonovelli.fi && sleep 90 && bin/contents_feeder_redis --site=pillu.pornonovelli.fi && sleep 90 && bin/contents_feeder_redis --site=chatti.pornonovelli.fi
+
+bin/contents_feeder_redis --site=posliinipillu.fi && sleep 90 && bin/contents_feeder_redis --site=video.posliinipillu.fi && sleep 90 && bin/contents_feeder_redis --site=chatti.posliinipillu.fi
