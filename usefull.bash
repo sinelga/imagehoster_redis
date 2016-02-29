cadb20e54992e6512c06809e977fd01f44736d84
alex <support@mazurov.eu>
2016-02-29 03:42:50 +0200

    prod

--- usefull.bash
+++ usefull.bash
@@ -242,5 +242,6 @@ bin/contents_feeder_redis --site=nussivat.fi && sleep 90 && bin/contents_feeder_
 bin/contents_feeder_redis --site=ilmaispornoa.fi && sleep 90 && bin/contents_feeder_redis --site=kuvia.ilmaispornoa.fi && sleep 90 && bin/contents_feeder_redis --site=pillut.ilmaispornoa.fi
 bin/contents_feeder_redis --site=pornotarina.fi && sleep 90 && bin/contents_feeder_redis --site=ilmaista.pornotarina.fi && sleep 90 && bin/contents_feeder_redis --site=ilmainen.pornotarina.fi
 bin/contents_feeder_redis --site=rakasteluvideot.fi && sleep 90 && bin/contents_feeder_redis --site=lesbo.rakasteluvideot.fi && sleep 90 && bin/contents_feeder_redis --site=fb.rakasteluvideot.fi
-
 bin/contents_feeder_redis --site=seksiasennot.fi && sleep 90 && bin/contents_feeder_redis --site=vaimo.seksiasennot.fi && sleep 90 && bin/contents_feeder_redis --site=mies.seksiasennot.fi
+
+bin/contents_feeder_redis --site=pornoelokuva.fi && sleep 90 && bin/contents_feeder_redis --site=live.pornoelokuva.fi && sleep 90 && bin/contents_feeder_redis --site=kovaa.pornoelokuva.fi
